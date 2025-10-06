c91bb29e5e7d2e390aabd2fdbc380ee8808240de
Uros Popovic <urospopovic1996@gmail.com>
2025-10-06 22:43:30 +0200

    Initial commit

---
+++ week-1/about-me/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("My name is Uros, I am embedded software engineer and I like to play basketball in my spare time.");
+}
---
+++ week-1/hello-world/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("My name is Uros.");
+    println!("My favorite programming language is C.");
+    println!("I am learning Rust for the future. :)");
+}
