70d0b5ef7122a75b5d73976e7df9095607acdd42
Uros Popovic <urospopovic1996@gmail.com>
2025-10-06 22:52:11 +0200

    Add surname print statement

--- week-1/hello-world/src/main.rs
+++ week-1/hello-world/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("My name is Uros.");
+    println!("My surname is Popovic.");
     println!("My favorite programming language is C.");
     println!("I am learning Rust for the future. :)");
 }
